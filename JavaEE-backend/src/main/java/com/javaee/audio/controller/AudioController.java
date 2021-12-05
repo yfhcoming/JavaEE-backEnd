406bf65f32c2fcafedd1e8099384c081f7e5f548
Hylissang <1916297575@qq.com>
2021-12-05 20:39:42 +0800

    添加mapper配置

--- JavaEE-backend/src/main/java/com/javaee/audio/controller/AudioController.java
+++ JavaEE-backend/src/main/java/com/javaee/audio/controller/AudioController.java
@@ -1,6 +1,9 @@
 package com.javaee.audio.controller;
 
 
+import com.javaee.audio.service.AudioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/audio")
 public class AudioController {
+
+    @Autowired
+    AudioService audioService;
+
+    @GetMapping("/all")
+    public void getAllAudios()
+    {
+
+    }
+
 
 }
