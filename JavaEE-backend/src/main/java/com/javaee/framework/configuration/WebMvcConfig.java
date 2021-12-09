917caed686701b41bd7df738a5b6057b31560f2e
Hylissang <1916297575@qq.com>
2021-12-09 15:45:42 +0800

    跨域请求

---
+++ JavaEE-backend/src/main/java/com/javaee/framework/configuration/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.javaee.framework.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //配置允许跨域的路径
+        registry.addMapping("/**")
+                //配置允许访问的跨域资源的请求域名
+                .allowCredentials(true)
+                .allowedOrigins("*")
+                //配置允许访问该跨域资源服务器的请求方法
+                .allowedMethods("*")
+                //配置允许请求 头部head的访问
+                .allowedHeaders("*");
+    }
+}
